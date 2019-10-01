7298afdbe93901a9aa30757475c009ae0d519b13
dileepkm6 <dileepkm6@gmail.com>
2019-10-01 10:19:20 +0530

    replaced id with name

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,23 +1,19 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
 
 public class Main
 {
     public static void main( String[] args )
     {
-
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie_A=context.getBean("movie",Movie.class);
-        Movie movie_B=context.getBean("movie",Movie.class);
+        Movie movie_A=context.getBean("movieA",Movie.class);
+        Movie movie_B=context.getBean("movieB",Movie.class);
         System.out.println("Name :"+movie_A.getActor().getName()+" gender:"+movie_A.getActor().getGender()+" age:"+movie_A.getActor().getAge());
-        //using scope as prototype
+        //default scope
         System.out.println(movie_A==movie_B);
-
     }
 
 }
