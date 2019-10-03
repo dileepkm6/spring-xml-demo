f444ba05f0931864aa7591958ddbfa27487dd293
dileepkm6 <dileepkm6@gmail.com>
2019-10-03 09:24:38 +0530

    binpostprocessor added

--- src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
+++ src/main/java/com/stackroute/demo/BeanLifecycleDemoBean.java
@@ -2,6 +2,7 @@ package com.stackroute.demo;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class BeanLifecycleDemoBean implements InitializingBean, DisposableBean {
 
